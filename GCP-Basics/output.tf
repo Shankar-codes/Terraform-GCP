58c439c841f3333322fa2655d4fa1ddbf7184dfe
Shankar-codes <harikainteriors@gmail.com>
2026-08-10 14:26:51 +0530

    GCP Infrastructure

---
+++ GCP-Basics/output.tf
@@ -0,0 +1,11 @@
+output "instance_name" {
+  value = google_compute_instance.web.name
+}
+
+output "instance_public_ip" {
+  value = google_compute_instance.web.network_interface[0].access_config[0].nat_ip
+}
+
+output "subnet_name" {
+  value = google_compute_subnetwork.devops_subnet.name
+}
