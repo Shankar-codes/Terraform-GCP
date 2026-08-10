f5bc2589be10d41b1977a062581a1f2d8eac7340
Shankar-codes <harikainteriors@gmail.com>
2026-08-10 14:20:22 +0530

    GCP Infrastructure

--- GCP-Basics/vpc.tf
+++ GCP-Basics/vpc.tf
@@ -3,3 +3,11 @@ resource "google_compute_network" "devops_vpc" {
   name                    = "devops_vpc"
   auto_create_subnetworks = false
 }
+
+resource "google_compute_subnetwork" "devops_subnet" {
+  project                  = var.project_id
+  name                     = "devops_subnet"
+  ip_cidr_range            = "10.0.1.0/24"
+  region                   = var.region
+  network                  = google_compute_network.devops_vpc.id
+}
