dff2d295501508b35ea932f217c23bd508fce006
Shankar-codes <harikainteriors@gmail.com>
2026-08-10 14:22:58 +0530

    GCP Infrastructure

--- GCP-Basics/vpc.tf
+++ GCP-Basics/vpc.tf
@@ -1,9 +1,11 @@
+# VPC creation
 resource "google_compute_network" "devops_vpc" {
   project                 = var.project_id
   name                    = "devops_vpc"
   auto_create_subnetworks = false
 }
 
+# Subnetwork creation
 resource "google_compute_subnetwork" "devops_subnet" {
   project                  = var.project_id
   name                     = "devops_subnet"
@@ -11,3 +13,18 @@ resource "google_compute_subnetwork" "devops_subnet" {
   region                   = var.region
   network                  = google_compute_network.devops_vpc.id
 }
+
+# Firewall rule creation
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.devops_vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+
